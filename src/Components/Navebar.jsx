db22d7494a018817a20129a5a7a2d422e4daffff
AzharMehmood4 <azharzharmehmoodm@gmail.com>
2025-02-10 11:56:46 +0500

    update the navebar

--- src/Components/Navebar.jsx
+++ src/Components/Navebar.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
-import { FaInstagram, FaFacebookF, FaPinterestP, FaTwitter} from "react-icons/fa";
+import React from "react";
+import { FaInstagram, FaFacebookF, FaPinterestP, FaTwitter } from "react-icons/fa";
 
-const Navebar = () => {
+const Navbar = () => {
   return (
     <div className="bg-gray-100">
-        <div className="flex justify-between p-5">
-            {/* lef */}
-      <div> 
-        <h1>Travel Egency</h1>
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        {/* Left Section */}
+        <div className="text-xl font-bold text-gray-700">
+          TravelEgency
+        </div>
+
+        {/* Center Section - Links */}
+        <ul className="hidden md:flex space-x-6 text-gray-600">
+          <li className="hover:text-blue-600 cursor-pointer">Home</li>
+          <li className="hover:text-blue-600 cursor-pointer">About</li>
+          <li className="hover:text-blue-600 cursor-pointer">Travel</li>
+          <li className="hover:text-blue-600 cursor-pointer">Eat</li>
+          <li className="hover:text-blue-600 cursor-pointer">Relax</li>
+          <li className="hover:text-blue-600 cursor-pointer">Videos</li>
+        </ul>
+
+        {/* Right Section - Social Icons */}
+        <div className="flex space-x-4 text-gray-500">
+          <FaFacebookF className="hover:text-blue-600 cursor-pointer" />
+          <FaInstagram className="hover:text-blue-600 cursor-pointer" />
+          <FaPinterestP className="hover:text-blue-600 cursor-pointer" />
+          <FaTwitter className="hover:text-blue-600 cursor-pointer" />
+        </div>
       </div>
-      {/* list */}
-      <div className="flex justify-between">
-        <ul className="flex justify-between gap-x-5 text-gray-500 ">
-            <li className="hover:text-blue-600">Home</li>
-            <li className="hover:text-blue-600">About</li>
-            <li className="hover:text-blue-600">Travel</li>
-            <li className="hover:text-blue-600">Eat</li>
-            <li className="hover:text-blue-600">Relax</li>
-            <li className="hover:text-blue-600">videos</li>
+
+      {/* Responsive Menu */}
+      <div className="md:hidden flex justify-center mt-2">
+        <ul className="space-x-4 flex text-gray-600">
+          <li className="hover:text-blue-600 cursor-pointer">Home</li>
+          <li className="hover:text-blue-600 cursor-pointer">About</li>
+          <li className="hover:text-blue-600 cursor-pointer">Travel</li>
+          <li className="hover:text-blue-600 cursor-pointer">Eat</li>
+          <li className="hover:text-blue-600 cursor-pointer">Relax</li>
+          <li className="hover:text-blue-600 cursor-pointer">Videos</li>
         </ul>
       </div>
-      {/* right */}
-      <div className="flex justify-between">
-      <FaFacebookF color='gray'/>
-      <FaInstagram color="gray"/>
-      <FaPinterestP color='gray'/>
-      <FaTwitter color='gray'/>
-      <FaTwitter color='gray3456790'/>
-      </div>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navebar
+export default Navbar;
