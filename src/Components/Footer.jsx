f462f0497cf401d78d382b781f0bb9a8c93b47d8
AzharMehmood4 <azharzharmehmoodm@gmail.com>
2025-02-15 19:11:53 +0500

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
   return (
     <footer className="text-gray-600 body-font bg-gray-50">
       <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap md:text-left text-center order-first justify-between">            
+        <div className="flex flex-wrap md:text-left text-center order-first justify-center md:justify-between">            
               <div className="flex-col">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
                 Quick Links
@@ -55,7 +55,7 @@ const Footer = () => {
                   className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 />
               </div>
-              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-[#096BAC] border-0 py-2 px-6 focus:outline-none hover:bg-[#2F3E81] rounded">
+              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-[#096BAC] border-0 py-2 px-6 focus:outline-none hover:bg-[#2F3E81] rounded my-2 sm:my-0">
                 Subscribe
               </button>
             </div>
@@ -84,7 +84,7 @@ const Footer = () => {
               @chichienterprises
             </a>
           </p>
-        <div className="hidden md:flex space-x-4 text-gray-500">
+        <div className="flex space-x-4 text-gray-500 my-2">
           <FaFacebookF className="hover:text-blue-600 cursor-pointer" />
           <FaInstagram className="hover:text-pink-500 cursor-pointer" />
           <FaPinterestP className="hover:text-red-600 cursor-pointer" />
